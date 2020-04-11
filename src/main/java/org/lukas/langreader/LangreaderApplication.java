fd7edfb9b316497a8decdab7afd86829ebb8320c
Martin Lukáš <martinlukas24@protonmail.com>
2020-04-11 02:02:11 +0200

    Add properties for data source

--- src/main/java/org/lukas/langreader/LangreaderApplication.java
+++ src/main/java/org/lukas/langreader/LangreaderApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LangreaderApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(LangreaderApplication.class, args);
 	}
-
 }
