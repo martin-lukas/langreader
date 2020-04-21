e1cc9df2a3df2ed76aa0094d353066ed2d2f3f41
Martin Lukáš <martinlukas24@protonmail.com>
2020-04-21 18:02:52 +0200

    Remove Bootstrap and refactor API calls

--- frontend/src/components/Library.vue
+++ frontend/src/components/Library.vue
@@ -1,46 +1,36 @@
 <template>
-  <div id="library" class="rounded-lg">
-    <div>
-      <b-spinner label="Loading..." v-if="isLoading"></b-spinner>
-    </div>
-    <ul class="list-group">
-      <li class="list-group-item">
-        <LibraryItem v-for="text in texts" :key="text.id" :text="text"/>
-      </li>
-    </ul>
+  <div id="library">
+    <LibraryItem :textObjs="textObjs"
+                     @text-selected="selectText"
+                     @area-selected="changeArea"/>
   </div>
 </template>
 
 <script>
+  import api from '../utils/backend-api';
   import LibraryItem from "./LibraryItem";
-  import axios from "axios";
-
-  const SERVER_URL = 'http://localhost';
-  const UPDATE_TEXT_API = '/api/texts';
 
   export default {
     components: {LibraryItem},
     data() {
       return {
-        textAreaInput: '',
-        texts: [],
-        isLoading: false,
+        textObjs: [],
       }
     },
     methods: {
       fetchTexts() {
-        axios.get(`${SERVER_URL}${UPDATE_TEXT_API}`)
-          .then(response => {
-            this.isLoading = false;
-            this.texts = response.data;
-          })
-          .catch(err => {
+        api.getTextTitles().then(response => {
+          this.textObjs = response.data;
+        }).catch(err => {
             console.log(err);
           });
+      },
+      selectText(textObj) {
+        this.$emit('text-selected', textObj);
+      },
+      changeArea(contentArea) {
+        this.$emit('area-selected', contentArea);
       }
-    },
-    created() {
-      this.isLoading = true;
     },
     mounted() {
       this.fetchTexts();
