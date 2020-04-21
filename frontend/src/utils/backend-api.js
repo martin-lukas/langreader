e48b81355117c8c80c44100fb10b2d270e22b051
Martin Lukáš <martinlukas24@protonmail.com>
2020-04-22 01:16:18 +0200

    Fix bug in removing word - unexpected axios method params

--- frontend/src/utils/backend-api.js
+++ frontend/src/utils/backend-api.js
@@ -2,10 +2,10 @@ import axios from 'axios';
 
 const AXIOS = axios.create({
   // baseURL: 'http://18.185.128.105/api',
-  baseURL: 'http://localhost/api',
+  baseURL: 'http://localhost:8088/api',
   headers: {
     'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'POST, GET, OPTIONS, PUT, DELETE',
+    'Access-Control-Allow-Methods': 'POST, GET, PUT, DELETE',
     'Access-Control-Allow-Headers': 'Content-Type, X-Auth-Token, Origin, Authorization'
   }
 });
@@ -21,7 +21,7 @@ export default {
     return AXIOS.put('/words', wordObj);
   },
   removeWord(wordObj) {
-    return AXIOS.delete('/words', wordObj);
+    return AXIOS.delete('/words', {data: wordObj}); // different params for delete method...
   },
   getTextTitles() {
     return AXIOS.get('/texts');
