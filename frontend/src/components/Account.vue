b2fad88a982d280f6b3d10f731221bede4e17957
Martin Lukáš <martinlukas24@protonmail.com>
2020-04-21 18:08:50 +0200

    Init for an account page

---
+++ frontend/src/components/Account.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="account-page">
+    <h2>Account Page</h2>
+    <p>This is your account page. There is not much in here right now, but there will be a lot
+    of statistics in the future, and it will all be very interesting. You'll see. There will be
+    stuff like, how many words you know, which languages you're studying, now many texts you've
+    added, or go through all the words you're currently studying</p>
+  </div>
+</template>
+
+<style>
+  #account-page {
+  }
+
+  #account-page p {
+    padding: 0.8em 0;
+  }
+</style>
