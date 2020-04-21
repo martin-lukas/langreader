b099d9a29b95ce244435994c4d6c89f113385810
Martin Lukáš <martinlukas24@protonmail.com>
2020-04-22 01:11:42 +0200

    Rename LibraryItem to TextList (makes more sense)

---
+++ frontend/src/components/TextList.vue
@@ -0,0 +1,23 @@
+<template>
+  <ul id="text-list">
+    <li :text="textObj"
+        v-for="textObj in textObjs"
+        :key="textObj.id">
+      <a href="#"
+         @click.prevent="(e) => {
+             $emit('text-selected', textObj.id);
+             $emit('area-selected', 'reading');
+           }">
+        <p>{{ textObj.title }}</p>
+      </a>
+    </li>
+  </ul>
+</template>
+
+<script>
+  export default {
+    props: {
+      textObjs: Array
+    }
+  }
+</script>
