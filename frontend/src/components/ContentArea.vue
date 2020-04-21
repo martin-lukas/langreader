999e69f5c2ddeb09d05e0e27093f3a78699ac7df
Martin Lukáš <martinlukas24@protonmail.com>
2020-04-21 18:07:05 +0200

    Component for grouping content components

---
+++ frontend/src/components/ContentArea.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="content-area">
+    <Home v-if="selected === 'home'"/>
+    <Library v-else-if="selected === 'library'"
+             @text-selected="selectText"
+             @area-selected="changeArea"/>
+    <Account v-else-if="selected === 'account'"/>
+    <ReadingArea v-else-if="selected === 'reading'" :textObj="selectedTextObj"/>
+  </div>
+</template>
+
+<script>
+  import Home from './Home';
+  import Library from './Library';
+  import Account from './Account';
+  import ReadingArea from './ReadingArea';
+
+  export default {
+    components: {Home, Library, Account, ReadingArea},
+    props: {
+      selected: String
+    },
+    data() {
+      return {
+        selectedTextObj: null
+      }
+    },
+    methods: {
+      selectText(textObj) {
+        this.selectedTextObj = textObj;
+      },
+      changeArea(contentArea) {
+        this.$emit('area-selected', contentArea);
+      }
+    }
+  }
+</script>
+
+<style>
+  #content-area {
+    padding: 15px;
+  }
+</style>
