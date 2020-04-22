09c7e76e3e70ef6a349caa4c7b709a3612a197b0
Martin Lukáš <martinlukas24@protonmail.com>
2020-04-23 00:20:15 +0200

    Add a few more CSS resets

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -13,7 +13,7 @@
     components: {TopNav, ContentArea},
     data() {
       return {
-        selectedArea: 'library'
+        selectedArea: 'Library'
       }
     },
     methods: {
@@ -25,22 +25,33 @@
 </script>
 
 <style>
-  html, body, ul, li, a {
+  * {
+    box-sizing: border-box;
+  }
+
+  *:focus, *:visited, *:active, *:hover  { outline:0 !important;}
+  *::-moz-focus-inner {border:0;}
+
+  html, body, ul, li, a, button, textarea, input {
     margin: 0;
     padding: 0;
     border: 0;
     font-family: Arial, Helvetica, sans-serif;
   }
 
-  html, body {background-color: #CCC; height: 100vh}
+  a, p, h1, h2, h3, h4 {
+    color: #383838;
+  }
+
+  html, body {background-color: #f0f0f0;}
 
   ul, li {list-style: none}
 
   #container {
     margin: 0 auto;
     background-color: white;
+    border-radius: 0 0 10px 10px;
     max-width: 900px;
-    height: 100vh;
   }
 
   h2 {
