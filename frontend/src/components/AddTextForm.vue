93f0bd0d47583428f7e40cf2275707797463e087
Martin Lukáš <martinlukas24@protonmail.com>
2020-04-23 00:25:27 +0200

    Add text and search texts functionality added

---
+++ frontend/src/components/AddTextForm.vue
@@ -0,0 +1,113 @@
+<template>
+  <div id="add-text-form">
+    <div id="title-fields">
+      <label for="input-title">Title:</label>
+      <input type="text"
+             id="input-title"
+             v-model="titleInput"/>
+    </div>
+    <div id="text-fields">
+      <label for="input-text">Text:</label>
+      <textarea type="text"
+                id="input-text"
+                v-model="textInput"/>
+    </div>
+    <div id="add-button-div">
+      <button @click="sendAddText">Add</button>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        titleInput: '',
+        textInput: ''
+      }
+    },
+    methods: {
+      sendAddText() {
+        const trimmedTitle = this.titleInput.trim();
+        const trimmedText = this.textInput.trim();
+        if (trimmedTitle && trimmedText) {
+          this.$emit('add-text', {title: trimmedTitle, text: trimmedText});
+        }
+      }
+    },
+    mounted() {
+      document.getElementById('input-title').focus();
+    }
+  }
+</script>
+
+<style scoped>
+  #add-text-form {
+    text-align: left;
+    box-sizing: border-box;
+    border-radius: 5px;
+    background-color: #f2f2f2;
+    margin-top: 10px;
+    padding: 13px 13px;
+  }
+
+  #title-fields {
+    box-sizing: border-box;
+  }
+
+  #text-fields {
+    margin-top: 10px;
+    box-sizing: border-box;
+  }
+
+  input[type=text] {
+    font-size: 0.95em;
+    display: block;
+    box-sizing: border-box;
+    width: 100%;
+    padding: 8px;
+    border: 1px solid #b4b4b4;
+    border-radius: 5px;
+  }
+
+  input[type=text]:focus {
+    border: 2px solid #4994d4;
+    margin-bottom: -2px;
+  }
+
+  textarea {
+    font-size: 0.95em;
+    width: 100%;
+    height: 80px;
+    padding: 8px;
+    box-sizing: border-box;
+    border: 1px solid #b4b4b4;
+    border-radius: 5px;
+    resize: none;
+  }
+
+  textarea:focus {
+    border: 2px solid #4994d4;
+  }
+
+  #add-button-div {
+    text-align: right;
+  }
+
+  button {
+    font-size: 1em;
+    font-weight: bold;
+    background-color: #4994d4;
+    color: white;
+    margin-top: 10px;
+    padding: 8px 15px;
+    border: none;
+    border-radius: 5px;
+    outline: 0;
+    cursor: pointer;
+  }
+
+  button:hover, button:focus {
+    background-color: #327bbf;
+  }
+</style>
--- frontend/src/components/Library.vue
+++ frontend/src/components/Library.vue
@@ -1,6 +1,18 @@
 <template>
   <div id="library">
-    <TextList :textObjs="textObjs"
+    <div id="library-toolbar">
+      <input type="text"
+             v-model="searchQuery"
+             placeholder="Search.."
+             @keyup="filterTexts"/>
+      <button @click="toggleAddTextForm"
+              :class="(isToggledAddText) ? 'toggled' : ''">
+        +
+      </button>
+    </div>
+    <AddTextForm v-if="isToggledAddText"
+                 @add-text="addTextToDB"/>
+    <TextList :textObjs="shownTextObjs"
               @text-selected="selectText"
               @area-selected="changeArea"/>
   </div>
@@ -8,22 +20,43 @@
 
 <script>
   import api from '../utils/backend-api';
+  import AddTextForm from "./AddTextForm";
   import TextList from "./TextList";
 
   export default {
-    components: {TextList},
+    components: {AddTextForm, TextList},
     data() {
       return {
+        searchQuery: '',
         textObjs: [],
+        shownTextObjs: [],
+        isToggledAddText: false
       }
     },
     methods: {
       fetchTextTitles() {
         api.getTextTitles().then(response => {
           this.textObjs = response.data;
+          this.shownTextObjs = this.textObjs;
         }).catch(err => {
           console.error(err.response);
         });
+      },
+      filterTexts() {
+        this.shownTextObjs = this.textObjs.filter(textObj => {
+          return textObj.title.toLowerCase().includes(this.searchQuery.toLowerCase());
+        });
+      },
+      addTextToDB(textObj) {
+        this.isToggledAddText = false;
+        api.addText(textObj).then(response => {
+          this.fetchTextTitles();
+        }).catch(err => {
+          console.error(err.response);
+        });
+      },
+      toggleAddTextForm() {
+        this.isToggledAddText = !this.isToggledAddText;
       },
       selectText(textId) {
         this.$emit('text-selected', textId);
@@ -38,5 +71,52 @@
   }
 </script>
 
-<style>
+<style scoped>
+  #library {
+    text-align: right;
+  }
+
+  #library-toolbar {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+  }
+
+  input[type=text] {
+    font-size: 0.95em;
+    display: block;
+    box-sizing: border-box;
+    width: 100%;
+    padding: 8px;
+    border: 1px solid #b4b4b4;
+    border-radius: 5px;
+    /*background-image: url('searchicon.png');*/
+    /*background-position: 10px 10px;*/
+    /*background-repeat: no-repeat;*/
+  }
+
+  input[type=text]:focus {
+    border: 2px solid #4994d4;
+    margin-bottom: -2px;
+  }
+
+  button {
+    font-size: 1.7em;
+    background-color: #4994d4;
+    color: white;
+    padding: 2px 10px;
+    margin-left: 10px;
+    border: none;
+    border-radius: 10px;
+    outline: 0;
+    cursor: pointer;
+  }
+
+  button:hover {
+    background-color: #327bbf;
+  }
+
+  button.toggled {
+    background-color: #0a569a;
+  }
 </style>
