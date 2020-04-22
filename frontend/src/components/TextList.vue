f7986cba64be2a6a68875eb7a8296c1c5a9b853a
Martin Lukáš <martinlukas24@protonmail.com>
2020-04-23 00:23:17 +0200

    Change text items from list-items to links

--- frontend/src/components/TextList.vue
+++ frontend/src/components/TextList.vue
@@ -1,17 +1,16 @@
 <template>
-  <ul id="text-list">
-    <li :text="textObj"
-        v-for="textObj in textObjs"
-        :key="textObj.id">
-      <a href="#"
-         @click.prevent="(e) => {
-             $emit('text-selected', textObj.id);
-             $emit('area-selected', 'reading');
-           }">
-        <p>{{ textObj.title }}</p>
-      </a>
-    </li>
-  </ul>
+  <div id="text-list">
+    <a :text="textObj"
+       v-for="textObj in textObjs"
+       :key="textObj.id"
+       href="#"
+       @click.prevent="(e) => {
+         $emit('text-selected', textObj.id);
+         $emit('area-selected', 'Reading');
+       }">
+      {{ textObj.title }}
+    </a>
+  </div>
 </template>
 
 <script>
@@ -21,3 +20,23 @@
     }
   }
 </script>
+
+<style scoped>
+  a {
+    display: block;
+    border: 1px solid #747474;
+    border-radius: 10px;
+    margin-top: 15px;
+    text-decoration: none;
+    padding: 12px;
+    overflow: hidden;
+    outline: 0;
+    font-size: 1.1em;
+    font-weight: bold;
+    text-align: left;
+  }
+
+  a:hover {
+    background-color: #f0f0f0;
+  }
+</style>
