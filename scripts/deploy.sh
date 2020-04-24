80a4338ff26813fd89e71e166281537a535bf052
Martin Lukáš <martinlukas24@protonmail.com>
2020-04-24 14:59:48 +0200

    Finish completely automated deployment
    The deployment script now includes:
    - backing up the current database and sending it to the server
    - building frontend as well as backend (with already fixed server URL
    for API calls)
    - sending the resulting JAR to the server as well
    - connecting to server, and killing previous running instance
    - refreshing the remote DB from backup
    - launching the new server instance in the background and logging into files
    - removing the DB backup files locally and remotely

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,41 +1,25 @@
 #!/bin/bash
-# This script builds and sends the project to the AWS server, and also updates the
-# PostgreSQL database there. After running this script, you can SSH to the server and
-# in the home dir, run the script ~/langreader/run_server.sh
-## this is not a part of the deploy.sh script, because if Intellij IDEA crashes, while this
-## script is still running, the remote ports and DB connections stay open, and further running
-## this script fails because the DB is protected from being overriden (still in use). Also the
-## 80 port is still occupied. Only restarting the server helps.
-
-# Get to the project root
-cd ..
 
 # Dump the langreader database
 pg_dump langreader > db_dump
 
 # Send the dump to server
 scp -i /home/martin/.ssh/amazon-ec2-server.pem \
-/home/martin/IdeaProjects/langreader/db_dump \
-ubuntu@ec2-18-185-128-105.eu-central-1.compute.amazonaws.com:~/langreader/
+       db_dump \
+       ubuntu@18.185.128.105:~/langreader/
 
-# Build frontend
-cd frontend/ || exit
-vue build
-
-# Build backend + frontend together
-cd ../backend/ || exit
-mvn clean package
+# Build frontend + backend
+./build.sh
 
 # Send the built jar to server
 scp -i /home/martin/.ssh/amazon-ec2-server.pem \
-/home/martin/IdeaProjects/langreader/backend/target/backend-0.0.1-SNAPSHOT.jar \
-ubuntu@ec2-18-185-128-105.eu-central-1.compute.amazonaws.com:~/langreader/
+       ../backend/target/backend-0.0.1-SNAPSHOT.jar \
+       ubuntu@18.185.128.105:~/langreader/
 
 # Prepare remote DB
-cd ..
 ssh -i /home/martin/.ssh/amazon-ec2-server.pem \
-ubuntu@ec2-18-185-128-105.eu-central-1.compute.amazonaws.com \
-'bash -s' < ./scripts/remote_db_prepare.sh
+       ubuntu@18.185.128.105 \
+       'bash -s' < ./remote-run.sh
 
 # Remove the local DB dump
 rm db_dump
--- scripts/run_server.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sudo java -jar \
-	-Dserver.port=80 \
-	-Dspring.datasource.username="ubuntu" \
-	-Dspring.datasource.password="ubuntu" \
-	backend-0.0.1-SNAPSHOT.jar
