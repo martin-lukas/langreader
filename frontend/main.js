f28ce8b61135430b9fce446c971193417e971e91
Martin Lukáš <martinlukas24@protonmail.com>
2020-04-24 15:01:46 +0200

    Add some TODOs for future reference

--- frontend/main.js
+++ frontend/main.js
@@ -3,6 +3,10 @@ import App from './src/App';
 import router from './router';
 import store from './store';
 
+// TODO: fix 404 when accessing URL /reading/12trtrer or /reading/121654 etc.
+// TODO: favicon disappears (404) when accessing something nonexistent...
+// TODO: add loading circle when parsing reading text
+
 Vue.config.productionTip = false
 
 new Vue({
