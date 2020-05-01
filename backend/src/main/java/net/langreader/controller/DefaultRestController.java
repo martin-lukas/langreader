697d8d847012a4c01d364c504e4bcfb9bbff9ebf
Martin Lukáš <martinlukas24@protonmail.com>
2020-05-01 22:44:59 +0200

    Fix loading of /error page when navigating to not-found page

--- backend/src/main/java/net/langreader/controller/DefaultRestController.java
+++ backend/src/main/java/net/langreader/controller/DefaultRestController.java
@@ -7,11 +7,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
-public class DefaultRestController {
+public class DefaultRestController implements ErrorController {
     // Forwards all routes to FrontEnd except: '/', '/index.html', '/api', '/api/**'
     // Required because of 'mode: history' usage in frontend routing, see README for further details
     @RequestMapping(value = "{_:^(?!index\\.html|api).*$}")
     public String redirect() {
+        return "forward:/";
+    }
+
+    @Override
+    public String getErrorPath() {
         return "forward:/";
     }
 }
