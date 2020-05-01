bcb234b18320df1c5bd23c1c7ff4ee5b30a02a57
Martin Lukáš <martinlukas24@protonmail.com>
2020-05-01 12:42:05 +0200

    Add login support (redirect)

---
+++ frontend/src/views/Account.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="account-page">
+    <h2>Hi, {{ currentUser.username }}!</h2>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'account',
+    computed: {
+      currentUser() {
+        return this.$store.state.auth.user;
+      }
+    },
+    mounted() {
+      if (!this.currentUser) {
+        this.$router.push('/login');
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  #account-page {
+  }
+
+  p {
+    padding: 0.8em 0;
+  }
+</style>
