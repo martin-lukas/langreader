eda2259afa66ecece85199e3e3b271c084bf117c
Martin Lukáš <martinlukas24@protonmail.com>
2020-05-01 12:18:06 +0200

    Move classes from security package to more approp.

---
+++ backend/src/main/java/net/langreader/model/Role.java
@@ -0,0 +1,20 @@
+package net.langreader.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "roles")
+@Getter @Setter @NoArgsConstructor @ToString
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Enumerated(EnumType.STRING)
+    private ERole name;
+}
