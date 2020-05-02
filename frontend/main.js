cb280cd7ec3f4814e698a181ede8c8cd258a54d8
Martin Lukáš <martinlukas24@protonmail.com>
2020-05-02 22:45:47 +0200

    Remove vee-validate use, and some todos

--- frontend/main.js
+++ frontend/main.js
@@ -1,19 +1,12 @@
 import Vue from 'vue';
-import VeeValidate from 'vee-validate';
 import App from './src/App';
 import router from './src/router';
 import store from './src/store';
 
-// TODO: again happened that repeated word disappeared but focus didnt move after...
+// TODO: limit input stream (ByteStreams.limit() from Guava or BoundedInputStream from Apache Commons)
 // TODO: start logging which user is doing what
-// TODO: EXAMINE SQL INJECTION WITH ' CHARS IN WORDS
-// TODO: add loading circle when parsing reading text
-// TODO: add a text length limit (stop it before it gets sent to server)
-// - also... maybe limit how much can be put inside the text area, and the input field
 
 Vue.config.productionTip = false
-
-Vue.use(VeeValidate);
 
 new Vue({
   router,
