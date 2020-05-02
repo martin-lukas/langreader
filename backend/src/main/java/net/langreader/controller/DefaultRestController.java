3df5372d2f61034feb4cad4f58571c9faf3c8595
Martin Lukáš <martinlukas24@protonmail.com>
2020-05-02 22:46:30 +0200

    Remove comment and unused imports

--- backend/src/main/java/net/langreader/controller/DefaultRestController.java
+++ backend/src/main/java/net/langreader/controller/DefaultRestController.java
@@ -2,14 +2,11 @@ package net.langreader.controller;
 
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class DefaultRestController implements ErrorController {
     // Forwards all routes to FrontEnd except: '/', '/index.html', '/api', '/api/**'
-    // Required because of 'mode: history' usage in frontend routing, see README for further details
     @RequestMapping(value = "{_:^(?!index\\.html|api).*$}")
     public String redirect() {
         return "forward:/";
