65fb0edfb6c918cafcfc6b4e9f43179154f7d7e3
Martin Lukáš <martinlukas24@protonmail.com>
2020-05-05 13:30:01 +0200

    Change design to list of divs

--- frontend/src/views/Language.vue
+++ frontend/src/views/Language.vue
@@ -9,38 +9,23 @@
       </option>
     </select>
     <button @click="addNewLanguage">Add</button>
-    <table>
-      <thead>
-      <tr>
-        <th>Code</th>
-        <th>Languge</th>
-        <th>Action</th>
-      </tr>
-      </thead>
-      <tbody>
-      <tr v-for="lang in userLangs" :key="lang.id">
-        <td>
-          {{ lang.code }}
-        </td>
-        <td>
-          <span v-if="lang.code !== chosenLang.code" class="lang-span"
-                @click="setCurrentLang(lang)">
-            {{ lang.fullName }}
-          </span>
-          <span v-else class="current-lang-span">
-            {{ lang.fullName }}
-          </span>
-        </td>
-        <td>
-          <a href="#"
-             v-if="lang.code !== chosenLang.code"
-             @click.prevent="removeUserLang(lang)">
-            X
-          </a>
-        </td>
-      </tr>
-      </tbody>
-    </table>
+
+    <div class="lang-item-div"
+         :class="isChosenLang(lang) ? 'chosen-lang-div' : ''"
+         v-for="lang in userLangs"
+         :key="lang.id">
+      <a href="#"
+         class="lang-item-link"
+         :class="isChosenLang(lang) ? 'chosen-lang-item-link' : ''"
+         @click.prevent="setCurrentLang(lang)">
+        {{ lang.fullName }}
+      </a>
+      <button v-if="!isChosenLang(lang)"
+              class="lang-item-del-btn"
+              @click="removeUserLang(lang)">
+        <i class="fas fa-ban"></i>
+      </button>
+    </div>
   </div>
 </template>
 
@@ -85,6 +70,9 @@
           }
         }
         return false;
+      },
+      isChosenLang(lang) {
+        return lang.id === this.chosenLang.id;
       }
     }
   }
@@ -108,14 +96,74 @@
     background-color: var(--active-el-color-dark);
   }
 
-  .lang-span {
-    color: var(--active-el-color-dark);
+  .lang-item-div {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    border: 1px solid #747474;
+    border-radius: 10px;
+    margin-top: 15px;
+    text-align: left;
+
+  }
+
+  .lang-item-del-btn {
+    margin: 0;
+    font-size: 1.1em;
+    background-color: white;
+    color: var(--input-border-gray-dark);
+    padding: 2px 8px;
+    border-color: var(--input-border-gray);
+    border-width: 0 0 0 1px;
+    border-style: solid;
+    outline: 0;
     cursor: pointer;
   }
 
-  .current-lang-span {
-    color: #016db3;
-    font-weight: bold;
+  .lang-item-del-btn {
+    border-radius: 0 10px 10px 0;
   }
 
+  .lang-item-edit-btn:hover {
+    background-color: #fffbaa;
+    color: #c9b502;
+  }
+
+  .lang-item-del-btn:hover {
+    background-color: #f5d0c9;
+    color: #dc0b0b;
+  }
+
+  .lang-item-link {
+    display: block;
+    width: 100%;
+    padding: 12px;
+    font-size: 1.1em;
+    font-weight: bold;
+    text-align: left;
+    text-decoration: none;
+    overflow: hidden;
+    border-radius: 10px 0 0 10px;
+    color: var(--default-text-color);
+    outline: 0;
+  }
+
+  .lang-item-link:hover {
+    background-color: var(--input-border-gray-light);
+  }
+
+  .chosen-lang-div {
+    border-color: var(--active-el-color);
+    border-width: 2px;
+  }
+
+  .chosen-lang-item-link {
+    cursor: default;
+    color: var(--active-el-color-dark);
+    border-radius: 10px;
+  }
+
+  .chosen-lang-item-link:hover {
+    background-color: white;
+  }
 </style>
