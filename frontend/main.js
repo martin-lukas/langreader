96693bab1d66c176cb83b411f328b4b381668e61
Martin Lukáš <martinlukas24@protonmail.com>
2020-05-05 13:28:57 +0200

    Update todos

--- frontend/main.js
+++ frontend/main.js
@@ -3,8 +3,8 @@ import App from './src/App';
 import router from './src/router';
 import store from './src/store';
 
+// TODO: choose translation languages
 // TODO: limit input stream (ByteStreams.limit() from Guava or BoundedInputStream from Apache Commons)
-// TODO: start logging which user is doing what
 
 Vue.config.productionTip = false
 
