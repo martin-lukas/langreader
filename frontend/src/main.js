6c484bc6667d5fe1b7bad1afe980684225866221
Martin Lukáš <martinlukas24@protonmail.com>
2020-08-16 15:15:26 +0200

    Move main.js to enable building with npm

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
-import App from './src/App';
-import router from './src/router';
-import store from './src/store';
+import App from './App';
+import router from './router';
+import store from './store';
 
 // TODO: choose translation languages
 // TODO: limit input stream (ByteStreams.limit() from Guava or BoundedInputStream from Apache Commons)
